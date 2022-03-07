37af13580a51154caa0bd1e37815d63527314ddf
Tommaso Zazzaretti <93591433+Tommaso-Zazzaretti@users.noreply.github.com>
2022-03-07 09:56:45 +0100

    Delete Angular/app directory

--- Angular/app/Models/Token.ts
+++
@@ -1,6 +0,0 @@
-export class Token{
-    public token:string|null = null;
-    public constructor(tokenString:string|null){
-        this.token = tokenString;
-    }
-}
--- Angular/app/Models/UserCredentials.ts
+++
@@ -1,9 +0,0 @@
-export class UserCredentials {
-    public Username:string|null = null;
-    public Password:string|null = null;
-
-    public constructor(username:string|null, password:string|null){
-        this.Username = username;
-        this.Password = password;
-    }
-}
