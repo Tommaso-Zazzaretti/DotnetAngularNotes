d0382d41cf878721b7e0c533fdef98e1a691030a
Tommaso Zazzaretti <93591433+Tommaso-Zazzaretti@users.noreply.github.com>
2022-03-07 09:52:55 +0100

    Add files via upload

---
+++ Dotnet/Startup.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using static DotNet6Mediator.ApplicationLayer.ApplicationLayerConfiguration;
+using static DotNet6Mediator.InfrastructureLayer.InfrastructureLayerConfiguration;
+
+namespace DotNet6Mediator
+{
+    public class Startup
+    {
+        public IConfigurationRoot Configuration { get; }
+
+        public Startup(IConfigurationRoot configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddInfrastructureLayer(this.Configuration); 
+            services.AddApplicationLayer();
+            
+        }
+
+        public void Configure(WebApplication app)
+        {
+            app.UseApplicationLayer();
+        }
+    }
+}
